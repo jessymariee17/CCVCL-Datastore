4c426387cb380020a26e154b839c3eb163a8855f
maureen <maureenhm@gmail.com>
2014-08-13 14:33:03 -0400

    my message

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ class GetAllPoint(webapp2.RequestHandler):
             self.response.write(",")
             self.response.write(point.z)
             self.response.write("\n")
+#my random comment
                         
 app = webapp2.WSGIApplication([
     ('/', MainHandler),    
