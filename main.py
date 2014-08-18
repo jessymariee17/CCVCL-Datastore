eb5e39beaf0c6c4adace357090f37d27b3ef2de5
maureen <maureenhm@gmail.com>
2014-08-18 12:49:59 -0400

    Minor comment change

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class GetAllPoint(webapp2.RequestHandler):
             self.response.write(",")
             self.response.write(point.z)
             self.response.write("\n")
-#hello greg
+
                         
 app = webapp2.WSGIApplication([
     ('/', MainHandler),    
